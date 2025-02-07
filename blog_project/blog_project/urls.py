4149fc2ae6d1c9b146d88ffb5e2a8448bd08de57
Haroon966 <haroon.orenda@gmail.com>
2025-02-07 16:04:00 +0500

    Updated files for Simple Blog Project

--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from blog.views import home  # Import the home view
 
 urlpatterns = [
-    path('', home, name='home'),  # Add this line to route the home view
+    path('', include('blog.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include('blog.urls')),
 ]
